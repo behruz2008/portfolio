ff41b307d1d3a025c216e02bd4bdcb43f263faf4
behruz2008 <usmonovbehruz371@gmail.com>
2023-03-30 11:30:05 +0500

    Deploy

--- project/settings.py
+++ project/settings.py
@@ -108,11 +108,11 @@ USE_TZ = True
 
 import os
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-if DEBUG:
-    STATICFILES_DIRS =[
-        BASE_DIR /'static'
-    ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+# if DEBUG:
+#     STATICFILES_DIRS =[
+#         BASE_DIR /'static'
+#     ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
